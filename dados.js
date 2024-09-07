5557f039f1be0a921dbee0441c49f977cf12a935
Gabriel dos Santos <gabrieldsc2356@gmail.com>
2024-09-07 00:07:10 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,30 @@
+let dados = [
+    {
+        titulo:  "Michael Jordan",
+        descricao: "Michael Jordan, conhecido como o Air Jordan, é considerado por muitos o maior jogador de basquete de todos os tempos.Sua carreira brilhante nos Chicago Bulls, marcada por seis campeonatos da NBA, o tornou um ícone mundial do esporte.",
+        link:  "https://pt.wikipedia.org/wiki/Michael_Jordan",
+        link: "https://www.instagram.com/jumpman23/",
+        tags: "Air 23"
+    },
+    {
+        titulo: "Lebron James",
+        descricao: "LeBron James é considerado um dos maiores jogadores de basquete de todos os tempos. Conhecido por sua versatilidade e longevidade, LeBron conquistou diversos títulos e recordes ao longo de sua carreira.",
+        link: "https://pt.wikipedia.org/wiki/LeBron_James",
+        link: "https://www.instagram.com/kingjames/",
+        tags: "king"
+    },
+    {
+        titulo: "Kobe Bryant",
+        descricao: "Kobe Bryant, falecido em 2020, foi um ícone do basquete e um dos maiores artilheiros da história da NBA. Sua dedicação ao jogo e sua rivalidade com LeBron James marcaram uma era.",
+        link: "https://pt.wikipedia.org/wiki/Kobe_Bryant",
+        link: "https://www.instagram.com/kobebryant/",
+        tags: "mamba 24"
+    },
+    {
+        titulo: "Stephen curry",
+        descricao: "Stephen Curry revolucionou o jogo com sua habilidade de arremessar de três pontos. Ele liderou o Golden State Warriors a múltiplos campeonatos e é considerado um dos maiores arremessadores da história.",
+        link: "https://pt.wikipedia.org/wiki/Stephen_Curry",
+        link: "https://www.instagram.com/stephencurry30/",
+        tags: "3"
+    }
+];
